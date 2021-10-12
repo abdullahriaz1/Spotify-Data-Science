93d34b176a988aeb6d13fd9ea9d0fb0c7f9db623
Abdullah Riaz <ariaz988@gmail.com>
2021-10-11 22:09:55 -0700

    .

--- auth.py
+++ auth.py
@@ -3,8 +3,8 @@ import json
 
 class Auth(object):
     def __init__(self):
-        self._client_id = "d20a5664636142e4ba58a7cbe3d4f3ea"
-        self._client_secret = "5a99d697c466440899e2cb4a94a4f5bb"
+        self._client_id = ""
+        self._client_secret = ""
         self._redirect_uri = "https://open.spotify.com/user/ariaz988"
         self._scope = "playlist-modify-private%20playlist-modify-public%20user-top-read"
     
